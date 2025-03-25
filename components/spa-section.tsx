3c8b99a482514d9e559c982cd5067688699c9f2a
nilohamburg <niclas@nilo-hamburg.de>
2025-03-25 20:27:42 +0100

    Update spa-section.tsx

--- components/spa-section.tsx
+++ components/spa-section.tsx
@@ -22,7 +22,7 @@ export function SpaSection() {
 
           <div className="order-1 lg:order-2 relative">
             <div className="relative aspect-[4/3] w-full overflow-hidden rounded-lg">
-              <Image src="/placeholder.svg?height=600&width=800" alt="Luxury spa pool" fill className="object-cover" />
+              <Image src="https://le-cdn.website-editor.net/s/4126dc9806774f8188f699b389e985fb/dms3rep/multi/opt/2023-09-Groneberger-Fr-4509-1920w.jpg?Expires=1743677346&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9sZS1jZG4ud2Vic2l0ZS1lZGl0b3IubmV0L3MvNDEyNmRjOTgwNjc3NGY4MTg4ZjY5OWIzODllOTg1ZmIvZG1zM3JlcC9tdWx0aS9vcHQvMjAyMy0wOS1Hcm9uZWJlcmdlci1Gci00NTA5LTE5MjB3LmpwZyIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc0MzY3NzM0Nn19fV19&Signature=k56U56fLmzejaViRUouQZNuKqste7o7bbHBd~bGnlpBZ72ZVDvW3vHvd4-N8x8vYvVAO~Ja0rXIJKshHKaJ2oeACGKXglI1fcGV-lcGV-fql2goe8VSEdig7e5MhDp-ubYbITRgM6xBH6GKeqOJzgB9O9KyRvjrqGmgCtMPqx5X6nVZgAj-qcnYQ0PQmwmA7OpmDzR~XYgWot-FUhDCEBqnIZoJyltGXxSyAtKLNFLtutgDl8vUhusPeu4BBOrLkSADHoFGu6sW~TIBIMr5AfG4a1gu25B25dwJ5GwnImXEJ51eLSeuyya3zeVedu8QmiP9jPVPGLgt~lGtLOZBXOw__&Key-Pair-Id=K2NXBXLF010TJW" alt="Luxury spa pool" fill className="object-cover" />
             </div>
           </div>
         </div>
